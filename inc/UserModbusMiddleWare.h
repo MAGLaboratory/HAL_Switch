2681704c0269ef43b5e9457b6c406ebdf85cb393
B. Kirisaki <b.k.lu@ieee.org>
2025-01-22 01:29:18 -0800

    Forgot to add user configuration file.

---
+++ inc/UserModbusMiddleWare.h
@@ -0,0 +1,20 @@
+/*
+ * UserModbusMiddleWare.h
+ *
+ *  Created on: 2025-01-20
+ *      Author: Brandon
+ *
+ * This file is to be included in ModbusMiddleWare.h.
+ */
+
+#ifndef INC_USERMODBUSMIDDLEWARE_H_
+#define INC_USERMODBUSMIDDLEWARE_H_
+
+/* Define the kind of struct that the modbus middleware is supposed to use
+ *
+ * MMW_STRUCT_EXTERNAL lets the user define structs and use multiple structs
+ * MMW_STRUCT_INTERNAL defines the structs automatically  
+ */
+#define MMW_STRUCT_TYPE MMW_STRUCT_INTERNAL
+
+#endif /* INC_USERMODBUSMIDDLEWARE_H_ */
